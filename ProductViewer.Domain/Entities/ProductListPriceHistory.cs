0f84430a39c5bf79703e95dd0689f4a6cad3fd97
User <User@WIN-GQOKRARBRRP>
2018-09-06 11:43:09 +0300

    Added new entity ProductListPriceHistory

---
+++ ProductViewer.Domain/Entities/ProductListPriceHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProductViewer.Domain.Entities
+{
+    public class ProductListPriceHistory // Changes in the list price of a product over time.
+    {
+        public int ProductID { get; set; } // Product identification number. Foreign key to Product.ProductID.
+
+        public DateTime StartDate { get; set; } // List price start date.
+
+        public DateTime EndDate { get; set; } // List price end date.
+
+        public decimal ListPrice { get; set; } // Product list price.
+    }
+}
