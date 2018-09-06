08367745ab161dfc80695f18b89541071dfa914d
User <User@WIN-GQOKRARBRRP>
2018-09-06 12:42:42 +0300

    Added new reference to Domain project. Created and initialize entities at HomeController

--- ProductViewer.WebUI/Controllers/HomeController.cs
+++ ProductViewer.WebUI/Controllers/HomeController.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProductViewer.Domain.Abstract;
+using ProductViewer.Domain.Concrete;
 
 namespace ProductViewer.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private IProductsRepository _products;
+        private IProductDescriptionsRepository _productDescriptions;
+        private IProductInventoriesRepository _productInventories;
+        private IProductListPriceHistoriesRepository _productListPriceHistories;
+
+        public HomeController()
+        {
+            var productInfoContext = new ProductInfoContext();
+            _products = new ProductRepository(productInfoContext);
+            _productDescriptions = new ProductDescriptionRepository(productInfoContext);
+            _productInventories = new ProductInventoryRepository(productInfoContext);
+            _productListPriceHistories = new ProductListPriceHistoryRepository(productInfoContext);
+        }
+
         public ActionResult Index()
         {
             return View();
