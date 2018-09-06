c45fd8c9c017d02841ca3bf114586756b93871b5
User <User@WIN-GQOKRARBRRP>
2018-09-06 12:21:31 +0300

    Created new class ProductRepository which implements interface IProductRepository

---
+++ ProductViewer.Domain/Concrete/ProductRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using ProductViewer.Domain.Abstract;
+using ProductViewer.Domain.Entities;
+
+namespace ProductViewer.Domain.Concrete
+{
+    public class ProductRepository : IProductsRepository
+    {
+        private ProductInfoContext _context;
+
+        public ProductRepository(ProductInfoContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetProductList()
+        {
+            return _context.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        public void Create(Product item)
+        {
+            _context.Products.Add(item);
+        }
+
+        public void Update(Product item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            Product book = _context.Products.Find(id);
+            if (book != null)
+                _context.Products.Remove(book);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool _disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this._disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
