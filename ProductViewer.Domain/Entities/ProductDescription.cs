da4b96e6854242135dc541b94ded08a950d21c40
User <User@WIN-GQOKRARBRRP>
2018-09-06 11:37:36 +0300

    Added new Entity ProductDescription and comment for description entity Product

---
+++ ProductViewer.Domain/Entities/ProductDescription.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductViewer.Domain.Entities
+{
+    public class ProductDescription // Product descriptions in several languages.
+    {
+        public int ProductDescriptionID { get; set; } // Primary key for ProductDescription records.
+        public string Description { get; set; } // Description of the product
+    }
+}
