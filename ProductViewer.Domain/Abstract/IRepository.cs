d184a15b951d7efcd9324c3a994889af37cb84e8
User <User@WIN-GQOKRARBRRP>
2018-09-06 17:40:55 +0300

    Added generic interface IRepository<T>

---
+++ ProductViewer.Domain/Abstract/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProductViewer.Domain.Abstract
+{
+    public interface IRepository<T> where T: class
+    {
+        IEnumerable<T> GetItemList(); // получение всех объектов
+
+        void Create(T item); // создание объекта
+
+        void Update(T item); // обновление объекта
+
+        void Save();  // сохранение изменений
+    }
+}
