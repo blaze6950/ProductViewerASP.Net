413cc16d91e286d059600f7ac22c8c3823d7a4dd
User <User@WIN-GQOKRARBRRP>
2018-09-07 13:28:16 +0300

    Deleted default created views. Added into Model folder new ViewModel for view model.

--- ProductViewer.WebUI/Controllers/HomeController.cs
+++ ProductViewer.WebUI/Controllers/HomeController.cs
@@ -15,22 +15,8 @@ namespace ProductViewer.WebUI.Controllers
             _unitOfWork = new UnitOfWork(productInfoContext);
         }
 
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ ProductViewer.WebUI/Models/ProductViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ProductViewer.Domain.Entities;
+
+namespace ProductViewer.WebUI.Models
+{
+    public class ProductViewModel
+    {
+        public ProductViewModel(Product productEntity, ProductDescription productDescriptionEntity, ProductInventory productInventoryEntity, ProductListPriceHistory productListPriceHistoryEntity)
+        {
+            ProductEntity = productEntity;
+            ProductDescriptionEntity = productDescriptionEntity;
+            ProductInventoryEntity = productInventoryEntity;
+            ProductListPriceHistoryEntity = productListPriceHistoryEntity;
+        }
+
+        #region PrivateFields
+        private Product ProductEntity { get; }
+        private ProductDescription ProductDescriptionEntity { get; }
+        private ProductInventory ProductInventoryEntity { get; }
+        private ProductListPriceHistory ProductListPriceHistoryEntity { get; }
+        #endregion
+
+        #region PublicFieldsForView
+        public string ProductName
+        {
+            get => ProductEntity.Name;
+        }
+
+        public string ProductDescription
+        {
+            get => ProductDescriptionEntity.Description;
+        }
+
+        public decimal ProductPrice
+        {
+            get => ProductListPriceHistoryEntity.ListPrice;
+        }
+
+        public int ProductQuantity
+        {
+            get => ProductInventoryEntity.Quantity;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            // продукты, затем их описание, стоимость и количество на складе
+            return $"Product: {ProductEntity.Name}, Description: {ProductDescriptionEntity.Description}, Price: {ProductListPriceHistoryEntity.ListPrice}, Count: {ProductInventoryEntity.Quantity}";
+        }
+    }
+}
