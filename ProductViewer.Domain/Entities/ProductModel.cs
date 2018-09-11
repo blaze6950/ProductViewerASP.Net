ebbbd96f1543332c5179639e282d11570cddc9ac
User <User@WIN-GQOKRARBRRP>
2018-09-11 11:42:52 +0300

    Added new entities for reference product to description

---
+++ ProductViewer.Domain/Entities/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductViewer.Domain.Entities
+{
+    public class ProductModel // Product model classification.
+    {
+        public int ProductModelID { get; set; } // Primary key for ProductModel records
+        public string Name { get; set; } // Product model description
+    }
+}
---
+++ ProductViewer.Domain/Entities/ProductModelProductDescriptionCulture.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductViewer.Domain.Entities
+{
+    public class ProductModelProductDescriptionCulture // Cross-reference table mapping product descriptions and the language the description is written in.
+    {
+        public int ProductModelID { get; set; } // Primary key. Foreign key to ProductModel.ProductModelID.
+        public int ProductDescriptionID { get; set; } // Primary key. Foreign key to ProductDescription.ProductDescriptionID.
+        public string CultureID { get; set; } // Culture identification number. Foreign key to Culture.CultureID
+    }
+}
