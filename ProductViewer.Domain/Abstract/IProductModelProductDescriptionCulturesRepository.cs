fd099225a1a18d7e15b0b9e7b7854ed8292c8f09
User <User@WIN-GQOKRARBRRP>
2018-09-11 11:48:11 +0300

    Added new interfaces repository pattern for new entities

---
+++ ProductViewer.Domain/Abstract/IProductModelProductDescriptionCulturesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ProductViewer.Domain.Entities;
+
+namespace ProductViewer.Domain.Abstract
+{
+    public interface IProductModelProductDescriptionCulturesRepository
+    {
+        IEnumerable<ProductModelProductDescriptionCulture> GetProductModelProductDescriptionCultureList(); // получение всех объектов
+
+        ProductModelProductDescriptionCulture GetProductModelProductDescriptionCulture(int id); // получение одного объекта по id
+
+        void Create(ProductModelProductDescriptionCulture item); // создание объекта
+
+        void Update(ProductModelProductDescriptionCulture item); // обновление объекта
+
+        void Delete(int id); // удаление объекта по id
+    }
+}
---
+++ ProductViewer.Domain/Abstract/IProductModelsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ProductViewer.Domain.Entities;
+
+namespace ProductViewer.Domain.Abstract
+{
+    public interface IProductModelsRepository
+    {
+        IEnumerable<ProductModel> GetProductModelList(); // получение всех объектов
+
+        ProductModel GetProductModel(int id); // получение одного объекта по id
+
+        void Create(ProductModel item); // создание объекта
+
+        void Update(ProductModel item); // обновление объекта
+
+        void Delete(int id); // удаление объекта по id
+    }
+}
