fa440109393bc9aa7992ac69b95e1c7f94826323
User <User@WIN-GQOKRARBRRP>
2018-09-17 11:51:27 +0300

    Added new class for configure and view sorting products. Include property of that class into model Index view. Using razor expressions for viewing sort config

---
+++ ProductViewer.WebUI/Models/SortConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace ProductViewer.WebUI.Models
+{
+    /// <summary>
+    /// Class SortConfig for set configuration of sorting. That class help view curresnt sort config at Index view and help send config from client to server
+    /// </summary>
+    public class SortConfig
+    {
+        /// <summary>
+        /// Name sort if true asc, else desc. Default value = true
+        /// </summary>
+        public bool? Name { get; set; } = true;
+
+        /// <summary>
+        /// UnitPrice sort if true asc, else desc. Default value = null
+        /// </summary>
+        public bool? UnitPrice { get; set; } = null;
+        /// <summary>
+        /// Quantity sort if true asc, else desc. Default value = null
+        /// </summary>
+        public bool? Quantity { get; set; } = null;
+        /// <summary>
+        /// PriceForAll sort if true asc, else desc. Default value = null
+        /// </summary>
+        public bool? PriceForAll { get; set; } = null;
+    }
+}
