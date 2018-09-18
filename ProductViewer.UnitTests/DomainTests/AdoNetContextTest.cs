d25651a8eb8f7c67133900cc36b172b1a20f3e1d
User <User@WIN-GQOKRARBRRP>
2018-09-18 13:44:06 +0300

    Created folders for test Domain and WebUI projects. Added new test class for testing AdoNetContext class at Domain project

---
+++ ProductViewer.UnitTests/DomainTests/AdoNetContextTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductViewer.UnitTests.DomainTests
+{
+    [TestClass]
+    public class AdoNetContextTest
+    {
+        [TestMethod]
+        public void ProductDataTableIsNotNull()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
