6cb4338d8a73b76c26e845a0897ed2d3d9773ed6
User <User@WIN-GQOKRARBRRP>
2018-09-18 13:59:48 +0300

    Added test methods for testing behavior of AdoNetContext class at Domain project

--- ProductViewer.UnitTests/DomainTests/AdoNetContextTest.cs
+++ ProductViewer.UnitTests/DomainTests/AdoNetContextTest.cs
@@ -1,20 +1,82 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductViewer.Domain.Concrete;
 
 namespace ProductViewer.UnitTests.DomainTests
 {
     [TestClass]
     public class AdoNetContextTest
     {
+        private AdoNetContext _context;
+
+        [TestInitialize]
+        public void SetupTests()
+        {
+            //Arrange
+            _context = new AdoNetContext("Data Source=WIN-GQOKRARBRRP;Initial Catalog=AdventureWorks2014;Integrated Security=True");
+        }
+
         [TestMethod]
         public void ProductDataTableIsNotNull()
         {
-            //Arrange
-
             //Act
-
+            var products = _context.GetProducts();
             //Assert
+            Assert.IsNotNull(products);
+        }
 
+        [TestMethod]
+        public void ProductDescriptionDataTableIsNotNull()
+        {
+            //Act
+            var productDescriptions = _context.GetProductDescriptions();
+            //Assert
+            Assert.IsNotNull(productDescriptions);
+        }
+
+        [TestMethod]
+        public void ProductInventoryDataTableIsNotNull()
+        {
+            //Act
+            var productInventories = _context.GetProductInventories();
+            //Assert
+            Assert.IsNotNull(productInventories);
+        }
+
+        [TestMethod]
+        public void ProductListPriceHistoryDataTableIsNotNull()
+        {
+            //Act
+            var productListPriceHistories = _context.GetProductListPriceHistories();
+            //Assert
+            Assert.IsNotNull(productListPriceHistories);
+        }
+
+        [TestMethod]
+        public void ProductModelDataTableIsNotNull()
+        {
+            //Act
+            var productModels = _context.GetProductModels();
+            //Assert
+            Assert.IsNotNull(productModels);
+        }
+
+        [TestMethod]
+        public void CommitChangesMethodSuccesExecution()
+        {
+            //Act
+            _context.CommitChanges();
+            //Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void RefreshDataMethodSuccesExecution()
+        {
+            //Act
+            _context.RefreshData();
+            //Assert
+            Assert.IsTrue(true);
         }
     }
 }
