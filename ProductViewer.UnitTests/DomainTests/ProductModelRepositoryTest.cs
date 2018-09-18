358426fd5386cd5af20cabac234e6e7e5f50bc0a
User <User@WIN-GQOKRARBRRP>
2018-09-18 15:07:46 +0300

    Added new class ProductModelRepositoryTest for testing ProductModelRepository class

---
+++ ProductViewer.UnitTests/DomainTests/ProductModelRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductViewer.Domain.Abstract;
+using ProductViewer.Domain.Concrete;
+
+namespace ProductViewer.UnitTests.DomainTests
+{
+    [TestClass]
+    public class ProductModelRepositoryTest
+    {
+        private IProductModelsRepository _productModelsRepository;
+
+        [TestInitialize]
+        public void SetupTests()
+        {
+            //Arrange
+            _productModelsRepository = new ProductModelRepository(new AdoNetContext("Data Source=WIN-GQOKRARBRRP;Initial Catalog=AdventureWorks2014;Integrated Security=True"));
+        }
+
+        [TestMethod]
+        public void ProductModelListIsNotNull()
+        {
+            //Act
+            var list = _productModelsRepository.GetProductModelList();
+            //Asset
+            Assert.IsNotNull(list);
+        }
+
+        [TestMethod]
+        public void ProductModelIsNotNull()
+        {
+            //Act
+            var product = _productModelsRepository.GetProductModel(59);
+            //Asset
+            Assert.IsNotNull(product);
+        }
+    }
+}
