0b667a19bcd990b8aac13bef09e3e2f87fa5d739
User <User@WIN-GQOKRARBRRP>
2018-09-19 10:39:47 +0300

    Successfully created unit tests and tested behavior of repository ProductModel class

--- ProductViewer.UnitTests/DomainTests/ProductModelRepositoryTest.cs
+++ ProductViewer.UnitTests/DomainTests/ProductModelRepositoryTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using ProductViewer.Domain.Abstract;
 using ProductViewer.Domain.Concrete;
+using ProductViewer.Domain.Entities;
 
 namespace ProductViewer.UnitTests.DomainTests
 {
@@ -9,12 +12,70 @@ namespace ProductViewer.UnitTests.DomainTests
     public class ProductModelRepositoryTest
     {
         private IProductModelsRepository _productModelsRepository;
+        private Mock<IAdoNetContext> _mock;
+        private DataTable _productModelDataTable;
 
         [TestInitialize]
         public void SetupTests()
         {
             //Arrange
-            _productModelsRepository = new ProductModelRepository(new AdoNetContext("Data Source=WIN-GQOKRARBRRP;Initial Catalog=AdventureWorks2014;Integrated Security=True"));
+            _mock = new Mock<IAdoNetContext>();
+            _productModelDataTable = new DataTable()
+            {
+                Columns =
+                {
+                    new DataColumn("ProductModelID", typeof(int)){AutoIncrement = true, AutoIncrementSeed = 1, AutoIncrementStep = 1},
+                    new DataColumn("Name", typeof(string))
+                }
+            };
+            _productModelDataTable.Rows.Add(1, "Kayak");
+            _productModelDataTable.Rows.Add(2, "Lodka");
+            _productModelDataTable.Rows.Add(3, "Sudno");
+            _mock.Setup(c => c.GetProductModels()).Returns(_productModelDataTable);
+            _mock.Setup(c => c.CommitChanges());
+            _productModelsRepository = new ProductModelRepository(_mock.Object);
+        }
+
+        [TestMethod]
+        public void Create_ProductModelClassObjectPassed()
+        {
+            //Arrange
+            int countRowsExcepted = _productModelDataTable.Rows.Count + 1;
+            var newProductModel = new ProductModel() {Name = "Test", ProductModelID = 4};
+            //Act
+            _productModelsRepository.Create(newProductModel);
+            //Assert
+            _mock.Verify(c => c.GetProductModels());
+            _mock.Verify(c => c.CommitChanges());
+            Assert.AreEqual(countRowsExcepted, _productModelDataTable.Rows.Count);
+        }
+
+        [TestMethod]
+        public void Delete_ProductModelClassObjectPassed()
+        {
+            //Arrange
+            int countRowsExcepted = _productModelDataTable.Rows.Count - 1;
+            //Act
+            _productModelsRepository.Delete(1);
+            //Assert
+            Assert.AreEqual(countRowsExcepted, _productModelDataTable.Rows.Count);
+            _mock.Verify(c => c.CommitChanges());
+        }
+
+        [TestMethod]
+        public void Update_ProductModelClassObjectPassed()
+        {
+            //Arrange
+            var product = _productModelsRepository.GetProductModel(1);
+            var oldName = product.Name;
+            var newName = "NewName";
+            product.Name = newName;
+            //Act
+            _productModelsRepository.Update(product);
+            //Assert
+            _mock.Verify(c => c.GetProductModels());
+            _mock.Verify(c => c.CommitChanges());
+            Assert.AreNotEqual(oldName, _productModelsRepository.GetProductModel(1).Name);
         }
 
         [TestMethod]
@@ -30,7 +91,7 @@ namespace ProductViewer.UnitTests.DomainTests
         public void ProductModelIsNotNull()
         {
             //Act
-            var product = _productModelsRepository.GetProductModel(59);
+            var product = _productModelsRepository.GetProductModel(1);
             //Asset
             Assert.IsNotNull(product);
         }
