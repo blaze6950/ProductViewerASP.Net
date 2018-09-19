55b07cf6554aa68a208f4e64c0c1bd0055736761
User <User@WIN-GQOKRARBRRP>
2018-09-19 11:57:16 +0300

    Successfully created unit tests and tested behavior of ProductInventoryRepository class

--- ProductViewer.UnitTests/DomainTests/ProductInventoryRepositoryTest.cs
+++ ProductViewer.UnitTests/DomainTests/ProductInventoryRepositoryTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using ProductViewer.Domain.Abstract;
 using ProductViewer.Domain.Concrete;
+using ProductViewer.Domain.Entities;
 
 namespace ProductViewer.UnitTests.DomainTests
 {
@@ -9,12 +12,73 @@ namespace ProductViewer.UnitTests.DomainTests
     public class ProductInventoryRepositoryTest
     {
         private IProductInventoriesRepository _productInventoriesRepository;
+        private Mock<IAdoNetContext> _mock;
+        private DataTable _productInventoryDataTable;
 
         [TestInitialize]
         public void SetupTests()
         {
             //Arrange
-            _productInventoriesRepository = new ProductInventoryRepository(new AdoNetContext("Data Source=WIN-GQOKRARBRRP;Initial Catalog=AdventureWorks2014;Integrated Security=True"));
+            _mock = new Mock<IAdoNetContext>();
+            _productInventoryDataTable = new DataTable()
+            {
+                Columns =
+                {
+                    new DataColumn("Bin", typeof(byte)),
+                    new DataColumn("LocationID", typeof(Int16)),
+                    new DataColumn("ProductID", typeof(int)),
+                    new DataColumn("Quantity", typeof(Int16)),
+                    new DataColumn("Shelf", typeof(string))
+                }
+            };
+            _productInventoryDataTable.Rows.Add(1, 2, 5, 23, "A");
+            _productInventoryDataTable.Rows.Add(2, 3, 6, 34, "B");
+            _productInventoryDataTable.Rows.Add(3, 4, 7, 45, "C");
+            _mock.Setup(c => c.GetProductInventories()).Returns(_productInventoryDataTable);
+            _mock.Setup(c => c.CommitChanges());
+            _productInventoriesRepository = new ProductInventoryRepository(_mock.Object);
+        }
+
+        [TestMethod]
+        public void Create_ProductInventoryClassObjectPassed()
+        {
+            //Arrange
+            int countRowsExcepted = _productInventoryDataTable.Rows.Count + 1;
+            var newProductInventory = new ProductInventory() { Quantity = 56, ProductID = 8, LocationID = 5, Shelf = "D", Bin = 4};
+            //Act
+            _productInventoriesRepository.Create(newProductInventory);
+            //Assert
+            _mock.Verify(c => c.GetProductInventories());
+            _mock.Verify(c => c.CommitChanges());
+            Assert.AreEqual(countRowsExcepted, _productInventoryDataTable.Rows.Count);
+        }
+
+        [TestMethod]
+        public void Delete_ProductInventoryClassObjectPassed()
+        {
+            //Arrange
+            int countRowsExcepted = _productInventoryDataTable.Rows.Count - 1;
+            //Act
+            _productInventoriesRepository.Delete(2, 5);
+            //Assert
+            Assert.AreEqual(countRowsExcepted, _productInventoryDataTable.Rows.Count);
+            _mock.Verify(c => c.CommitChanges());
+        }
+
+        [TestMethod]
+        public void Update_ProductInventoryClassObjectPassed()
+        {
+            //Arrange
+            var productInventory = _productInventoriesRepository.GetProductInventory(2, 5);
+            var oldShelf = productInventory.Shelf;
+            var newShelf = "D";
+            productInventory.Shelf = newShelf;
+            //Act
+            _productInventoriesRepository.Update(productInventory);
+            //Assert
+            _mock.Verify(c => c.GetProductInventories());
+            _mock.Verify(c => c.CommitChanges());
+            Assert.AreNotEqual(oldShelf, _productInventoriesRepository.GetProductInventory(2, 5).Shelf);
         }
 
         [TestMethod]
@@ -30,7 +94,7 @@ namespace ProductViewer.UnitTests.DomainTests
         public void ProductInventoryIsNotNull()
         {
             //Act
-            var productInventory = _productInventoriesRepository.GetProductInventory(1, 1);
+            var productInventory = _productInventoriesRepository.GetProductInventory(2, 5);
             //Asset
             Assert.IsNotNull(productInventory);
         }
