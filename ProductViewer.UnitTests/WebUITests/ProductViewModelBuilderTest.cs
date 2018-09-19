314d1385327cd1c32ab51e4fbe473514348b6cd2
User <User@WIN-GQOKRARBRRP>
2018-09-19 17:14:14 +0300

    Small fixes at ProductViewModel class fro testing. Successfully created unit tests and tested behavior of ProductViewModelBuilder class

---
+++ ProductViewer.UnitTests/WebUITests/ProductViewModelBuilderTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProductViewer.WebUI.Models;
+
+namespace ProductViewer.UnitTests.WebUITests
+{
+    [TestClass]
+    public class ProductViewModelBuilderTest
+    {
+        private Mock<ProductViewModel> _mock;
+        private ProductViewModelBuilder _builder;
+
+        [TestInitialize]
+        public void SetupTests()
+        {
+            //Arrange
+            _mock = new Mock<ProductViewModel>();
+            _mock.SetupGet(c => c.ProductEntityId).Returns(1);
+            _mock.SetupGet(c => c.ProductDescriptionEntityDescription).Returns("TestDescription");
+            _mock.SetupGet(c => c.ProductDescriptionEntityProductDescriptionID).Returns(1);
+            _mock.SetupGet(c => c.ProductEntityDaysToManufacture).Returns(10);
+            _mock.SetupGet(c => c.ProductEntityListPrice).Returns(22.22m);
+            _mock.SetupGet(c => c.ProductEntityName).Returns("Test");
+            _mock.SetupGet(c => c.ProductEntityNumber).Returns("TestNumber");
+            _mock.SetupGet(c => c.ProductEntityReorderPoint).Returns(3);
+            _mock.SetupGet(c => c.ProductEntitySafetyStockLevel).Returns(5);
+            _mock.SetupGet(c => c.ProductEntitySellStartDate).Returns(DateTime.Today);
+            _mock.SetupGet(c => c.ProductEntityStandardCost).Returns(55.55m);
+            _mock.SetupGet(c => c.ProductInventoryEntityBin).Returns(2);
+            _mock.SetupGet(c => c.ProductInventoryEntityQuantity).Returns(23);
+            _mock.SetupGet(c => c.ProductInventoryEntityShelf).Returns("Z");
+            _mock.SetupGet(c => c.ProductModelEntityProductModelID).Returns(1);
+            _mock.SetupGet(c => c.ProductListPriceHistoryEntityListPrice).Returns(66.66m);
+            _mock.SetupGet(c => c.ProductListPriceHistoryEntityStartDate).Returns(DateTime.Today);
+            _mock.Setup(c => c.GetBuilder()).Returns(_builder);
+            _builder = new ProductViewModelBuilder(_mock.Object);
+        }
+
+        [TestMethod]
+        public void ProductEntityPropertyIsCorrectReturn()
+        {
+            //Act
+            var productEntity = _builder.ProductEntity;
+            //Assert
+            Assert.IsNotNull(productEntity);
+            Assert.AreEqual(productEntity.ProductId, _mock.Object.ProductEntityId);
+            Assert.AreEqual(productEntity.Name, _mock.Object.ProductEntityName);
+            Assert.AreEqual(productEntity.ProductNumber, _mock.Object.ProductEntityNumber);
+            Assert.AreEqual(productEntity.SafetyStockLevel, _mock.Object.ProductEntitySafetyStockLevel);
+            Assert.AreEqual(productEntity.ReorderPoint, _mock.Object.ProductEntityReorderPoint);
+            Assert.AreEqual(productEntity.StandardCost, _mock.Object.ProductEntityStandardCost);
+            Assert.AreEqual(productEntity.ListPrice, _mock.Object.ProductEntityListPrice);
+            Assert.AreEqual(productEntity.DaysToManufacture, _mock.Object.ProductEntityDaysToManufacture);
+            Assert.AreEqual(productEntity.SellStartDate, _mock.Object.ProductEntitySellStartDate);
+            Assert.AreEqual(productEntity.ProductModelID, _mock.Object.ProductModelEntityProductModelID);
+        }
+
+        [TestMethod]
+        public void ProductDescriptionEntityPropertyIsCorrectReturn()
+        {
+            //Act
+            var productDescriptionEntity = _builder.ProductDescriptionEntity;
+            //Assert
+            Assert.IsNotNull(productDescriptionEntity);
+            Assert.AreEqual(productDescriptionEntity.ProductDescriptionID,
+                _mock.Object.ProductDescriptionEntityProductDescriptionID);
+            Assert.AreEqual(productDescriptionEntity.Description,
+                _mock.Object.ProductDescriptionEntityDescription);
+        }
+
+        [TestMethod]
+        public void ProductInventoryEntityPropertyIsCorrectReturn()
+        {
+            //Act
+            var productInventoryEntity = _builder.ProductInventoryEntity;
+            //Assert
+            Assert.IsNotNull(productInventoryEntity);
+            Assert.AreEqual(productInventoryEntity.ProductID, _mock.Object.ProductEntityId);
+            Assert.AreEqual(productInventoryEntity.LocationID, 1);
+            Assert.AreEqual(productInventoryEntity.Shelf, _mock.Object.ProductInventoryEntityShelf);
+            Assert.AreEqual(productInventoryEntity.Bin, _mock.Object.ProductInventoryEntityBin);
+            Assert.AreEqual(productInventoryEntity.Quantity, _mock.Object.ProductInventoryEntityQuantity);
+        }
+
+        [TestMethod]
+        public void ProductListPriceHistoryEntityPropertyIsCorrectReturn()
+        {
+            //Act
+            var productListPriceHistoryEntity = _builder.ProductListPriceHistoryEntity;
+            //Assert
+            Assert.IsNotNull(productListPriceHistoryEntity);
+            Assert.AreEqual(productListPriceHistoryEntity.ProductID, _mock.Object.ProductEntityId);
+            Assert.AreEqual(productListPriceHistoryEntity.StartDate, _mock.Object.ProductListPriceHistoryEntityStartDate);
+            Assert.AreEqual(productListPriceHistoryEntity.ListPrice, _mock.Object.ProductListPriceHistoryEntityListPrice);
+        }
+
+        [TestMethod]
+        public void ProductModelProductDescriptionCultureEntityPropertyIsCorrectReturn()
+        {
+            //Act
+            var productModelProductDescriptionCultureEntity = _builder.ProductModelProductDescriptionCultureEntity;
+            //Assert
+            Assert.IsNotNull(productModelProductDescriptionCultureEntity);
+            Assert.AreEqual(productModelProductDescriptionCultureEntity.ProductModelID, _mock.Object.ProductModelEntityProductModelID);
+            Assert.AreEqual(productModelProductDescriptionCultureEntity.ProductDescriptionID, _mock.Object.ProductDescriptionEntityProductDescriptionID);
+            Assert.AreEqual(productModelProductDescriptionCultureEntity.CultureID, "en");
+        }
+
+        [TestMethod]
+        public void ProductModelEntityPropertyIsCorrectReturn()
+        {
+            //Act
+            var productModelEntity = _builder.ProductModelEntity;
+            //Assert
+            Assert.IsNotNull(productModelEntity);
+            Assert.AreEqual(productModelEntity.ProductModelID, _mock.Object.ProductModelEntityProductModelID);
+            Assert.AreEqual(productModelEntity.Name, _mock.Object.ProductEntityName);
+        }
+
+        [TestMethod]
+        public void ProductViewModelPropertyIsCorrectReturn()
+        {
+            //Act
+            var productViewModel = _builder.ProductViewModel;
+            //Assert
+            Assert.IsNotNull(productViewModel);
+            Assert.AreEqual(productViewModel, _mock.Object);
+            Assert.AreNotSame(productViewModel, _mock.Object);
+        }
+    }
+}
--- ProductViewer.WebUI/Models/ProductViewModel.cs
+++ ProductViewer.WebUI/Models/ProductViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
-using ProductViewer.Domain.Entities;
+// ReSharper disable InconsistentNaming
+
+// ReSharper disable NonReadonlyMemberInGetHashCode
 
 namespace ProductViewer.WebUI.Models
 {
@@ -21,21 +23,21 @@ namespace ProductViewer.WebUI.Models
         }
         
         [HiddenInput(DisplayValue = false)]
-        public int ProductEntityId
+        public virtual int ProductEntityId
         {
             get;
             set;
         }
 
         [HiddenInput(DisplayValue = false)]
-        public int ProductModelEntityProductModelID
+        public virtual int ProductModelEntityProductModelID
         {
             get;
             set;
         }
 
         [HiddenInput(DisplayValue = false)]
-        public int ProductDescriptionEntityProductDescriptionID
+        public virtual int ProductDescriptionEntityProductDescriptionID
         {
             get;
             set;
@@ -44,7 +46,7 @@ namespace ProductViewer.WebUI.Models
         #region FiledsForAddOrEditingView
         [Required(ErrorMessage = "Please enter name of product")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "The name must be between 3 and 50 characters in length!")]
-        public string ProductEntityName
+        public virtual string ProductEntityName
         {
             get;
             set;
@@ -53,7 +55,7 @@ namespace ProductViewer.WebUI.Models
         [Required(ErrorMessage = "Please enter unique number of product")]
         [RegularExpression("[A-Z]{2}-([A-Z]\\d{3})|[A-Z]{2}-(\\d{4})", ErrorMessage = "Please enter a valid unique number of product")]
         [StringLength(25, MinimumLength = 7, ErrorMessage = "The name must be between 7 and 25 characters in length!")]
-        public string ProductEntityNumber
+        public virtual string ProductEntityNumber
         {
             get;
             set;
@@ -61,7 +63,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter safety stock level")]
         [Range(1, 32767, ErrorMessage = "The safety stock level must be positive and between 1 and 32767!")]
-        public Int16 ProductEntitySafetyStockLevel
+        public virtual Int16 ProductEntitySafetyStockLevel
         {
             get;
             set;
@@ -69,7 +71,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter reorder point")]
         [Range(1, 32767, ErrorMessage = "The reorder point must be positive and between 1 and 32767!")]
-        public Int16 ProductEntityReorderPoint
+        public virtual Int16 ProductEntityReorderPoint
         {
             get;
             set;
@@ -78,7 +80,7 @@ namespace ProductViewer.WebUI.Models
         [Required(ErrorMessage = "Please enter standard cost")]
         [Range(1, 922337203685477, ErrorMessage = "The standart cost must be positive!")]
         [DataType(DataType.Currency)]
-        public decimal ProductEntityStandardCost
+        public virtual decimal ProductEntityStandardCost
         {
             get;
             set;
@@ -87,7 +89,7 @@ namespace ProductViewer.WebUI.Models
         [Required(ErrorMessage = "Please enter list price")]
         [Range(0, 922337203685477, ErrorMessage = "The list price must be positive!")]
         [DataType(DataType.Currency)]
-        public decimal ProductEntityListPrice
+        public virtual decimal ProductEntityListPrice
         {
             get;
             set;
@@ -95,7 +97,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter days to manufacture")]
         [Range(0, 2147483647, ErrorMessage = "The list price must be positive and between 0 and 2147483647!")]
-        public int ProductEntityDaysToManufacture
+        public virtual int ProductEntityDaysToManufacture
         {
             get;
             set;
@@ -103,7 +105,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter sell start date")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ProductEntitySellStartDate
+        public virtual DateTime ProductEntitySellStartDate
         {
             get;
             set;
@@ -112,7 +114,7 @@ namespace ProductViewer.WebUI.Models
         [Required(ErrorMessage = "Please enter description")]
         [StringLength(400, MinimumLength = 1, ErrorMessage = "The description must be between 1 and 400 characters in length!")]
         [DataType(DataType.MultilineText)]
-        public string ProductDescriptionEntityDescription
+        public virtual string ProductDescriptionEntityDescription
         {
             get;
             set;
@@ -120,7 +122,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter shelf")]
         [StringLength(10, MinimumLength = 1, ErrorMessage = "The shelf must be between 1 and 400 characters in length!")]
-        public string ProductInventoryEntityShelf
+        public virtual string ProductInventoryEntityShelf
         {
             get;
             set;
@@ -128,7 +130,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter bin")]
         [Range(0, 100, ErrorMessage = "The bin must be between 0 and 100!")]
-        public byte ProductInventoryEntityBin
+        public virtual byte ProductInventoryEntityBin
         {
             get;
             set;
@@ -136,7 +138,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter quantity")]
         [Range(0, 32767, ErrorMessage = "The quantity must be between 0 and 32767!")]
-        public Int16 ProductInventoryEntityQuantity
+        public virtual Int16 ProductInventoryEntityQuantity
         {
             get;
             set;
@@ -144,7 +146,7 @@ namespace ProductViewer.WebUI.Models
 
         [Required(ErrorMessage = "Please enter start date")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ProductListPriceHistoryEntityStartDate
+        public virtual DateTime ProductListPriceHistoryEntityStartDate
         {
             get;
             set;
@@ -153,7 +155,7 @@ namespace ProductViewer.WebUI.Models
         [Required(ErrorMessage = "Please enter list price history")]
         [Range(0, 922337203685477, ErrorMessage = "The list price history must be positive!")]
         [DataType(DataType.Currency)]
-        public decimal ProductListPriceHistoryEntityListPrice
+        public virtual decimal ProductListPriceHistoryEntityListPrice
         {
             get;
             set;
@@ -168,13 +170,61 @@ namespace ProductViewer.WebUI.Models
             get { return ProductInventoryEntityQuantity * ProductListPriceHistoryEntityListPrice; }
         }
 
-        public ProductViewModelBuilder GetBuilder()
+        public override bool Equals(object obj)
+        {
+            var model = obj as ProductViewModel;
+            return model != null &&
+                   ProductEntityId == model.ProductEntityId &&
+                   ProductModelEntityProductModelID == model.ProductModelEntityProductModelID &&
+                   ProductDescriptionEntityProductDescriptionID == model.ProductDescriptionEntityProductDescriptionID &&
+                   ProductEntityName == model.ProductEntityName &&
+                   ProductEntityNumber == model.ProductEntityNumber &&
+                   ProductEntitySafetyStockLevel == model.ProductEntitySafetyStockLevel &&
+                   ProductEntityReorderPoint == model.ProductEntityReorderPoint &&
+                   ProductEntityStandardCost == model.ProductEntityStandardCost &&
+                   ProductEntityListPrice == model.ProductEntityListPrice &&
+                   ProductEntityDaysToManufacture == model.ProductEntityDaysToManufacture &&
+                   ProductEntitySellStartDate == model.ProductEntitySellStartDate &&
+                   ProductDescriptionEntityDescription == model.ProductDescriptionEntityDescription &&
+                   ProductInventoryEntityShelf == model.ProductInventoryEntityShelf &&
+                   ProductInventoryEntityBin == model.ProductInventoryEntityBin &&
+                   ProductInventoryEntityQuantity == model.ProductInventoryEntityQuantity &&
+                   ProductListPriceHistoryEntityStartDate == model.ProductListPriceHistoryEntityStartDate &&
+                   ProductListPriceHistoryEntityListPrice == model.ProductListPriceHistoryEntityListPrice &&
+                   PriceForAll == model.PriceForAll;
+        }
+
+        public virtual ProductViewModelBuilder GetBuilder()
         {
             if (_builder == null)
             {
                 _builder = new ProductViewModelBuilder(this);
             }
             return _builder;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -791488338;
+            hashCode = hashCode * -1521134295 + ProductEntityId.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductModelEntityProductModelID.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductDescriptionEntityProductDescriptionID.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProductEntityName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProductEntityNumber);
+            hashCode = hashCode * -1521134295 + ProductEntitySafetyStockLevel.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductEntityReorderPoint.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductEntityStandardCost.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductEntityListPrice.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductEntityDaysToManufacture.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductEntitySellStartDate.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProductDescriptionEntityDescription);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProductInventoryEntityShelf);
+            hashCode = hashCode * -1521134295 + ProductInventoryEntityBin.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductInventoryEntityQuantity.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductListPriceHistoryEntityStartDate.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductListPriceHistoryEntityListPrice.GetHashCode();
+            hashCode = hashCode * -1521134295 + PriceForAll.GetHashCode();
+            return hashCode;
         }
 
         public override string ToString()
