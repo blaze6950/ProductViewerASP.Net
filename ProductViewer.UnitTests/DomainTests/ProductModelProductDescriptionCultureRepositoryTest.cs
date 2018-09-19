d4cf2447838bf5821460f45012e84e8fd32c16ff
User <User@WIN-GQOKRARBRRP>
2018-09-19 11:33:23 +0300

    Successfully created unit tests and tested behavior of ProductListPriceHistoryRepository class

--- ProductViewer.UnitTests/DomainTests/ProductModelProductDescriptionCultureRepositoryTest.cs
+++ ProductViewer.UnitTests/DomainTests/ProductModelProductDescriptionCultureRepositoryTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using ProductViewer.Domain.Abstract;
 using ProductViewer.Domain.Concrete;
+using ProductViewer.Domain.Entities;
 
 namespace ProductViewer.UnitTests.DomainTests
 {
@@ -9,12 +12,71 @@ namespace ProductViewer.UnitTests.DomainTests
     public class ProductModelProductDescriptionCultureRepositoryTest
     {
         private IProductModelProductDescriptionCulturesRepository _productDescriptionCulturesRepository;
+        private Mock<IAdoNetContext> _mock;
+        private DataTable _productModelProductDescriptionCultureDataTable;
 
         [TestInitialize]
         public void SetupTests()
         {
             //Arrange
-            _productDescriptionCulturesRepository = new ProductModelProductDescriptionCultureRepository(new AdoNetContext("Data Source=WIN-GQOKRARBRRP;Initial Catalog=AdventureWorks2014;Integrated Security=True"));
+            _mock = new Mock<IAdoNetContext>();
+            _productModelProductDescriptionCultureDataTable = new DataTable()
+            {
+                Columns =
+                {
+                    new DataColumn("ProductDescriptionID", typeof(int)),
+                    new DataColumn("ProductModelID", typeof(int)),
+                    new DataColumn("CultureID", typeof(string))
+                }
+            };
+            _productModelProductDescriptionCultureDataTable.Rows.Add(1, 2, "en");
+            _productModelProductDescriptionCultureDataTable.Rows.Add(2, 3, "en");
+            _productModelProductDescriptionCultureDataTable.Rows.Add(3, 4, "en");
+            _mock.Setup(c => c.GetProductModelProductDescriptionCulture()).Returns(_productModelProductDescriptionCultureDataTable);
+            _mock.Setup(c => c.CommitChanges());
+            _productDescriptionCulturesRepository = new ProductModelProductDescriptionCultureRepository(_mock.Object);
+        }
+
+        [TestMethod]
+        public void Create_ProductModelProductDescriptionCultureClassObjectPassed()
+        {
+            //Arrange
+            int countRowsExcepted = _productModelProductDescriptionCultureDataTable.Rows.Count + 1;
+            var newProductModelProductDescriptionCulture = new ProductModelProductDescriptionCulture() { ProductModelID = 5, ProductDescriptionID = 4, CultureID = "en"};
+            //Act
+            _productDescriptionCulturesRepository.Create(newProductModelProductDescriptionCulture);
+            //Assert
+            _mock.Verify(c => c.GetProductModelProductDescriptionCulture());
+            _mock.Verify(c => c.CommitChanges());
+            Assert.AreEqual(countRowsExcepted, _productModelProductDescriptionCultureDataTable.Rows.Count);
+        }
+
+        [TestMethod]
+        public void Delete_ProductModelProductDescriptionCultureClassObjectPassed()
+        {
+            //Arrange
+            int countRowsExcepted = _productModelProductDescriptionCultureDataTable.Rows.Count - 1;
+            //Act
+            _productDescriptionCulturesRepository.Delete(2, 1);
+            //Assert
+            Assert.AreEqual(countRowsExcepted, _productModelProductDescriptionCultureDataTable.Rows.Count);
+            _mock.Verify(c => c.CommitChanges());
+        }
+
+        [TestMethod]
+        public void Update_ProductModelProductDescriptionCultureClassObjectPassed()
+        {
+            //Arrange
+            var productModelProductDescriptionCulture = _productDescriptionCulturesRepository.GetProductModelProductDescriptionCulture(2, 1);
+            var oldCultureID = productModelProductDescriptionCulture.CultureID;
+            var newCultureID = "ru";
+            productModelProductDescriptionCulture.CultureID = newCultureID;
+            //Act
+            _productDescriptionCulturesRepository.Update(productModelProductDescriptionCulture);
+            //Assert
+            _mock.Verify(c => c.GetProductModelProductDescriptionCulture());
+            _mock.Verify(c => c.CommitChanges());
+            Assert.AreEqual(oldCultureID, _productDescriptionCulturesRepository.GetProductModelProductDescriptionCulture(2, 1).CultureID);
         }
 
         [TestMethod]
@@ -30,7 +92,7 @@ namespace ProductViewer.UnitTests.DomainTests
         public void ProductModelProductDescriptionCultureIsNotNull()
         {
             //Act
-            var product = _productDescriptionCulturesRepository.GetProductModelProductDescriptionCulture(59, 744);
+            var product = _productDescriptionCulturesRepository.GetProductModelProductDescriptionCulture(2, 1);
             //Asset
             Assert.IsNotNull(product);
         }
