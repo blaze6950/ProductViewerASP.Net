2c37b9f16bff82ca3c915f3d630e6af3728cbb98
User <User@WIN-GQOKRARBRRP>
2018-10-01 23:12:40 +0300

    Added KendoUI into project with all references, successfully created and rendered bundles, all is correct

--- ProductViewer.WebUI/App_Start/BundleConfig.cs
+++ ProductViewer.WebUI/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace ProductViewer.WebUI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.3.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -16,7 +16,25 @@ namespace ProductViewer.WebUI
             bundles.Add(new StyleBundle("~/bundles/sitecss").Include(
                 "~/Content/scss/site.min.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/sitejs").Include("~/Scripts/materializejs/source/materialize.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/sitejs").Include(
+                "~/Scripts/materializejs/source/materialize.min.js"));
+
+            //Styles KendoUI
+            bundles.Add(new StyleBundle("~/bundles/kendo.common.material").Include(
+                "~/Content/kendo/2018.3.911/kendo.common-material.min.css"));
+            bundles.Add(new StyleBundle("~/bundles/kendo.mobile").Include(
+                "~/Content/kendo/2018.3.911/kendo.mobile.all.min.css"));
+            bundles.Add(new StyleBundle("~/bundles/kendo.material").Include(
+                "~/Content/kendo/2018.3.911/kendo.material.min.css"));
+            //Scripts KendoUI
+            bundles.Add(new ScriptBundle("~/bundles/jszip").Include(
+                "~/Scripts/kendo/2018.3.911/jszip.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendojs.all").Include(
+                "~/Scripts/kendo/2018.3.911/kendo.all.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendojs.aspnetmvc").Include(
+                "~/Scripts/kendo/2018.3.911/kendo.aspnetmvc.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendojs.modernizr.custom").Include(
+                "~/Scripts/kendo.modernizr.custom.js"));
         }
     }
 }
