0d50fa736fb8a94606243a5f5a7dd98ec59fb1f8
Nikita Zotov <d.progect21@gmail.com>
2018-10-02 15:35:03 +0300

    Kendo ui grid (#2)
    * Added KendoUI into project with all references, successfully created and rendered bundles, all is correct
    
    * Added kendo grid and referenced with server data
    
    * Fixed pagination and sorting items in table
    
    * Added action when clicked a row in main table, and open ProductDetails view
    
    * Added new feature for grid: filtering by name
    
    * Clean project and fix edit/remove buttons on each row in main table

--- ProductViewer.WebUI/ViewModels/ProductListViewModel.cs
+++ ProductViewer.WebUI/ViewModels/ProductListViewModel.cs
@@ -5,8 +5,5 @@ namespace ProductViewer.WebUI.Models
     public class ProductListViewModel
     {
         public IEnumerable<ProductViewModel> Products { get; set; }
-        public PagingInfo PagingInfo { get; set; }
-        public string SearchValue { get; set; }
-        public SortConfig SortConfig { get; set; }
     }
 }
