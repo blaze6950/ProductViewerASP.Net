46c3be12fe9cef8e0c85ad55521a3b905bd33479
User <User@WIN-GQOKRARBRRP>
2018-10-10 17:21:15 +0300

    Added new api controller and create logic for working with UOW

---
+++ ProductViewer.WebUI/WebApiControllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductViewer.Domain.Abstract;
+using ProductViewer.Domain.Concrete;
+using ProductViewer.WebUI.Models;
+
+namespace ProductViewer.WebUI.WebApiControllers
+{
+    public class ProductsController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private IEnumerable<ProductViewModel> _list;
+
+        public ProductsController()
+        {
+            _unitOfWork = new UnitOfWork(new ConnectionFactory(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString));
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<ProductViewModel> GetProducts()
+        {
+            if (_list == null)
+            {
+                InitialList();
+            }
+            return _list;
+        }
+
+        // GET: api/Products/5
+        [HttpGet]
+        public ProductViewModel GetProduct(int id)
+        {
+            return _list.FirstOrDefault(p=>p.ProductEntityId == id);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Create([FromBody]ProductViewModel value)
+        {
+            var builder = value.GetBuilder();
+            _unitOfWork.ProductModelsRepository.Create(builder.ProductModelEntity);
+            _unitOfWork.ProductsRepository.Create(builder.ProductEntity);
+            _unitOfWork.ProductInventoriesRepository.Create(builder.ProductInventoryEntity);
+            _unitOfWork.ProductListPriceHistoriesRepository.Create(builder.ProductListPriceHistoryEntity);
+            _unitOfWork.ProductDescriptionsRepository.Create(builder.ProductDescriptionEntity);
+            _unitOfWork.ProductModelProductDescriptionCulturesRepository.Create(builder.ProductModelProductDescriptionCultureEntity);
+            _unitOfWork.Commit();
+        }
+
+        // PUT: api/Products/5
+        [HttpPut]
+        public void Update([FromBody]ProductViewModel value)
+        {
+            var builder = value.GetBuilder();
+            _unitOfWork.ProductModelsRepository.Update(builder.ProductModelEntity);
+            _unitOfWork.ProductsRepository.Update(builder.ProductEntity);
+            _unitOfWork.ProductInventoriesRepository.Update(builder.ProductInventoryEntity);
+            _unitOfWork.ProductListPriceHistoriesRepository.Update(builder.ProductListPriceHistoryEntity);
+            _unitOfWork.ProductDescriptionsRepository.Update(builder.ProductDescriptionEntity);
+            _unitOfWork.ProductModelProductDescriptionCulturesRepository.Update(builder.ProductModelProductDescriptionCultureEntity);
+            _unitOfWork.Commit();
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var builder = GetProduct(id).GetBuilder();
+            _unitOfWork.ProductInventoriesRepository.Delete(builder.ProductInventoryEntity.LocationID, builder.ProductInventoryEntity.ProductID);
+            _unitOfWork.ProductListPriceHistoriesRepository.Delete(builder.ProductListPriceHistoryEntity.ProductID, builder.ProductListPriceHistoryEntity.StartDate);
+            _unitOfWork.ProductsRepository.Delete(builder.ProductEntity.ProductId);
+            _unitOfWork.ProductModelProductDescriptionCulturesRepository.Delete(builder.ProductModelProductDescriptionCultureEntity.ProductModelID, builder.ProductModelProductDescriptionCultureEntity.ProductDescriptionID);
+            _unitOfWork.ProductModelsRepository.Delete(builder.ProductModelEntity.ProductModelID);
+            _unitOfWork.ProductDescriptionsRepository.Delete(builder.ProductDescriptionEntity.ProductDescriptionID);
+            _unitOfWork.Commit();
+        }
+
+        private void InitialList()
+        {
+            _list = (from p in _unitOfWork.ProductsRepository.GetProductList()
+                join pm in _unitOfWork.ProductModelsRepository.GetProductModelList() on p.ProductModelID equals pm.ProductModelID
+                join pmpdc in _unitOfWork.ProductModelProductDescriptionCulturesRepository.GetProductModelProductDescriptionCultureList() on pm.ProductModelID equals pmpdc.ProductModelID
+                join pd in _unitOfWork.ProductDescriptionsRepository.GetProductDescriptionList() on pmpdc.ProductDescriptionID equals pd.ProductDescriptionID
+                join pi in _unitOfWork.ProductInventoriesRepository.GetProductInventoryList() on p.ProductId equals pi.ProductID
+                join plph in _unitOfWork.ProductListPriceHistoriesRepository.GetProductListPriceHistoryList() on p.ProductId equals plph.ProductID
+                select new ProductViewModelBuilder(p, pd, pi, plph, pmpdc, pm).ProductViewModel);
+        }
+    }
+}
