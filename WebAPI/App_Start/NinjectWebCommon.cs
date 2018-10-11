7206c8e05cc55b7cde7db53cfa03540e59b8c9cd
User <User@WIN-GQOKRARBRRP>
2018-10-11 14:03:31 +0300

    Added all required packages for configuration ninject and configured. Added default connection string into Web.config

---
+++ WebAPI/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using ProductViewer.Domain.Abstract;
+using ProductViewer.Domain.Concrete;
+using WebAPI;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace WebAPI
+{
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+            return kernel;
+        }
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            kernel.Bind<IConnectionFactory>().
+                To<ConnectionFactory>()
+                .WithConstructorArgument(typeof(string),
+                    ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        }
+    }
+}
